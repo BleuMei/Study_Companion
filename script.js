2c1eb3cba9f2873183dea26bbb1de509fe2e49d0
BleuMei <sandstormwindchimes@gmail.com>
2026-04-11 19:29:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-// Load points
-let points = localStorage.getItem("points") || 0;
-points = parseInt(points);
+let points = parseInt(localStorage.getItem("points")) || 0;
 
-// Display points if element exists
-const pointsDisplay = document.getElementById("points");
-if (pointsDisplay) {
-  pointsDisplay.innerText = points;
+function updatePointsDisplay() {
+  const el = document.getElementById("points");
+  if (el) el.innerText = points;
 }
 
-// Earn points
+function savePoints() {
+  localStorage.setItem("points", points);
+  updatePointsDisplay();
+}
+
 function earnPoints(amount) {
   points += amount;
-  localStorage.setItem("points", points);
-  alert("You earned " + amount + " points!");
+  savePoints();
+  alert("+" + amount + " points");
 }
 
-// Quiz logic
 function checkAnswer(answer) {
   const result = document.getElementById("result");
 
   if (answer === 4) {
-    result.innerText = "Correct! +10 points";
+    result.innerText = "Correct!";
     points += 10;
   } else {
     result.innerText = "Wrong answer.";
   }
 
-  localStorage.setItem("points", points);
+  savePoints();
 }
+
+updatePointsDisplay();
